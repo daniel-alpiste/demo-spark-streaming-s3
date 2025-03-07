7e9714a607bd30b33584aaf79ace1c14cce7198c
Daniel Alpiste <daniel.alpiste@pucp.edu.pe>
2025-03-06 20:25:39 -0500

    ref: agregando una linea de logging

--- streaming_pipe.py
+++ streaming_pipe.py
@@ -1,33 +1,41 @@
 from pyspark.sql.functions import col
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType
+import logging
 
-schema = StructType([
-    StructField("event_time", StringType(), True),
-    StructField("user_id", StringType(), True),
-    StructField("action", StringType(), True),
-    StructField("value", DoubleType(), True)
-])
+logger = logging.getLogger(__name__)
+
+schema = StructType(
+    [
+        StructField("event_time", StringType(), True),
+        StructField("user_id", StringType(), True),
+        StructField("action", StringType(), True),
+        StructField("value", DoubleType(), True),
+    ]
+)
 
 json_path = "s3a://test-streaming-12354325/streaming_jsons/"
 schema_path = "dbfs:/tmp/streaming_schema/"
 
-df_stream = (spark.readStream
-                  .format("cloudFiles")                   
-                  .option("cloudFiles.format", "json")     
-                  .option("cloudFiles.inferSchema", "true") 
-                  .option("cloudFiles.schemaLocation", schema_path)
-                  .schema(schema)
-                  .load(json_path))
+df_stream = (
+    spark.readStream.format("cloudFiles")
+    .option("cloudFiles.format", "json")
+    .option("cloudFiles.inferSchema", "true")
+    .option("cloudFiles.schemaLocation", schema_path)
+    .schema(schema)
+    .load(json_path)
+)
 
 checkpoint_path = "dbfs:/mnt/path_to_checkpoint/json_events_checkpoint"
 
 query = (
-    df_stream
-    .writeStream
-    .format("delta")
+    df_stream.writeStream.format("delta")
     .option("checkpointLocation", checkpoint_path)
     .outputMode("append")
-    .table("workspace.streaming_test.json_events_delta")  # <-- En vez de .start("ruta...")
+    .table(
+        "workspace.streaming_test.json_events_delta"
+    )  # <-- En vez de .start("ruta...")
 )
+
+logger.info("Iniciando el proceso de streaming")
 
 query.awaitTermination()
