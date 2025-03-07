dbedf153ebb3a725cae60bd06d35652c4b21f6cc
Daniel Alpiste <105952282+daniel-alpiste@users.noreply.github.com>
2025-03-06 20:44:03 -0500

    feat: agregada funcion de desarrollador b (#2)

---
+++ dev_b_script.py
@@ -0,0 +1,6 @@
+def dev_b_script_function():
+    print("Hello, world! Im a script from dev_b")
+
+
+if __name__ == "__main__":
+    dev_b_script_function()
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from dev_a_script import dev_a_script_function
+from dev_b_script import dev_b_script_function
 
 
 def main():
     dev_a_script_function()
+    dev_b_script_function()
+
+
+if __name__ == "__main__":
+    main()
