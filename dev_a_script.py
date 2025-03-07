24ead31143d46bbd89702a9b5e988b99be32fc9d
Daniel Alpiste <105952282+daniel-alpiste@users.noreply.github.com>
2025-03-06 20:39:44 -0500

    Agregando funcion a del dev a (#1)
    * feat: funcionalidad programador a
    
    * feat: codigo dev a

---
+++ dev_a_script.py
@@ -0,0 +1,6 @@
+def dev_a_script_function():
+    print("Hello, world! Im a script from dev_a")
+
+
+if __name__ == "__main__":
+    dev_a_script_function()
---
+++ main.py
@@ -0,0 +1,5 @@
+from dev_a_script import dev_a_script_function
+
+
+def main():
+    dev_a_script_function()
